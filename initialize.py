13bfdeb8b054eac1d97aefb8efc7e043f067f0e5
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-03-14 21:29:39 -0300

    script para inicializacao das tabelas referentes a campos do CCR

--- initialize.py
+++ initialize.py
@@ -1,15 +1,20 @@
-import pertmed_site.md_manager.macros
-from pertmed_site.md_manager.models import ItemTitle, ItemField
 
-ItemTitle.objects.clear()
-ItemField.objects.clear()
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import md_manager.macros
+from md_manager.models import ItemTitle, ItemField
+
+ItemTitle.objects.all().delete()
+ItemField.objects.all().delete()
 
 index = 0
-for (title in info_items):
+for title in md_manager.macros.info_itens:
     itt = ItemTitle(item_name=title)
     itt.save()
     
-    for(field in info_fields[index]):
+    for field in md_manager.macros.info_fields[index]:
         itf = ItemField(parent_item=itt, field_name=field) 
+        itf.save()
         
     index += 1
