92c7058e4a09502d194921bb5bb8de66abce530d
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-03-14 21:52:41 -0300

    removendo definicoes envolvendo modelos removidos de models.py

--- md_manager/admin.py
+++ md_manager/admin.py
@@ -1,25 +1,11 @@
-from pertmed_site.md_manager.models import Doctor, Item, Field, PhoneNumber
+from pertmed_site.md_manager.models import Doctor, PhoneNumber, ItemField
 from django.contrib import admin
 
 
 class FieldInline(admin.TabularInline):
-    model = Field
+    model = ItemField
     extra = 3
 
 class PhoneNumberInline(admin.TabularInline):
     model = PhoneNumber
     extra = 3
-
-class ItemInline(admin.TabularInline):
-    model = Item
-    extra = 3
-
-class ItemAdmin(admin.ModelAdmin):
-    inlines = [FieldInline]
-    list_display = ('name',)
-
-class MDAdmin(admin.ModelAdmin):
-    inlines = [ItemInline, PhoneNumberInline]
-
-admin.site.register(Item, ItemAdmin)
-admin.site.register(Doctor, MDAdmin)
