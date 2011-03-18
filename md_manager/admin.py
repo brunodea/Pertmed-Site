2fcc9cb2dc874dc3f3ed74dec00e503ee0e155fb
brunodea <brunodea@inf.ufsm.br>
2011-03-17 23:52:42 -0300

    Ajustes no admin.py

--- md_manager/admin.py
+++ md_manager/admin.py
@@ -1,8 +1,8 @@
-from pertmed_site.md_manager.models import Doctor, PhoneNumber, ItemField, ItemTitle, Notifications
+from pertmed_site.md_manager.models import Doctor, PhoneNumber, ItemField, Item, Notifications
 from django.contrib import admin
 
 admin.site.register(Doctor)
 admin.site.register(PhoneNumber)
+admin.site.register(Item)
 admin.site.register(ItemField)
-admin.site.register(ItemTitle)
 admin.site.register(Notifications)
