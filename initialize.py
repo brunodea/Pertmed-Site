eb698c3e28355664dee6199d292753ffdd8b225b
brunodea <brunodea@inf.ufsm.br>
2011-03-19 00:50:24 -0300

    Erro de sintaxe arrumado.

--- initialize.py
+++ initialize.py
@@ -1,17 +1,17 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-import md_manager.macros import infos_dic
+import md_manager.macros
 from md_manager.models import Item, ItemField
 
 Item.objects.all().delete()
 ItemField.objects.all().delete()
 
-for title in infos_dic.keys():
+for title in md_manager.macros.infos_dic.keys():
     item = Item(name=title)
     item.save()
     
-    for field in infos_dic[title]:
+    for field in md_manager.macros.infos_dic[title]:
         item_field = ItemField(item=item, name=field) 
         item_field.save()
-        
+
